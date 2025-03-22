aab80c060260df4aa107cb8599c72b484018c99e
udedesire <udesire20@gmail.com>
2025-03-22 11:11:22 +0100

    new commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const progressBar = document.querySelector(".progress_bar"),
+    progressTExt = document.querySelector(".progress_text");
+// used to select HTML elements from a webpage's Document Object Model (DOM) and store them in variables.
+const progress = (value) => {
+    const percentage = (value / time) * 100;
+    progressBar.style.width =
+}
